c4f9823962011486164dae29e6291959a1a6b13e
Tuxi Metal <lang.guillaume@gmail.com>
2017-08-16 19:12:05 +0200

    Updating the home route and getting the datas using the knowledge repository

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,8 @@
 <?php
 
-$app->get('/', function() {
+$app->get('/', function() use($app) {
+  $knowledges = $app['dao.knowledge']->findAll();
+  
   ob_start();
   require dirname(__DIR__) . '/resources/templates/view.php';
   $view = ob_get_clean();
