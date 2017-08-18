623265ae3697568da0f7ab4d0a09fff2720bb43b
Tuxi Metal <lang.guillaume@gmail.com>
2017-08-18 02:14:06 +0200

    Adding twig templates support

--- app/routes.php
+++ app/routes.php
@@ -3,9 +3,5 @@
 $app->get('/', function() use($app) {
   $knowledges = $app['dao.knowledge']->findAll();
   
-  ob_start();
-  require dirname(__DIR__) . '/resources/templates/view.php';
-  $view = ob_get_clean();
-  
-  return $view;
+  return $app['twig']->render('index.html.twig', ['knowledges' => $knowledges]);
 });
--- app/services.php
+++ app/services.php
@@ -1,12 +1,20 @@
 <?php
 
 use Silex\Provider\DoctrineServiceProvider;
+use Silex\Provider\TwigServiceProvider;
 use Tuxi\Portfolio\Repository\KnowledgeRepository;
 
 /*
  * Add the Doctrine service provider for database connection.
  */
 $app->register(new DoctrineServiceProvider());
+
+/**
+ * Add the Twig service provider
+ */
+$app->register(new TwigServiceProvider(), [
+  'twig.path' => dirname(__DIR__) . '/resources/templates'
+]);
 
 /*
  * Add the Knowledge repository.
