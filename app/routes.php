0e801dbb01e6c1cc3bee0765ac5b8ab4836cd485
Tuxi Metal <lang.guillaume@gmail.com>
2017-08-19 08:12:20 +0200

    Updating the home route and getting technologies data

--- app/routes.php
+++ app/routes.php
@@ -2,6 +2,9 @@
 
 $app->get('/', function() use($app) {
   $knowledges = $app['dao.knowledge']->findAll();
+  $technologies = $app['dao.technology']->findAll();
   
-  return $app['twig']->render('index.html.twig', ['knowledges' => $knowledges]);
+  return $app['twig']->render('index.html.twig',
+    ['knowledges' => $knowledges, 'technologies' => $technologies]
+  );
 });
