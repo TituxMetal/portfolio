c19bb72f300daa7448905cf644ea687726fdf199
Tuxi Metal <lang.guillaume@gmail.com>
2017-08-19 19:44:38 +0200

    Updating technologies seeder; adding data for image relation

--- storage/seeds/TechnologiesSeeder.php
+++ storage/seeds/TechnologiesSeeder.php
@@ -8,70 +8,87 @@ class TechnologiesSeeder extends AbstractSeed {
     $data = [
       [
         'title' => 'Html5',
+        'image' => 1,
         'created' => date('Y-m-d H:i:s')
       ],
       [
         'title' => 'Css3',
+        'image' => 2,
         'created' => date('Y-m-d H:i:s')
       ],
       [
         'title' => 'Javascript',
+        'image' => 3,
         'created' => date('Y-m-d H:i:s')
       ],
       [
         'title' => 'Php',
+        'image' => 4,
         'created' => date('Y-m-d H:i:s')
       ],
       [
         'title' => 'MariaDb',
+        'image' => 5,
         'created' => date('Y-m-d H:i:s')
       ],
       [
         'title' => 'Sass',
+        'image' => 6,
         'created' => date('Y-m-d H:i:s')
       ],
       [
         'title' => 'Atom',
+        'image' => 7,
         'created' => date('Y-m-d H:i:s')
       ],
       [
         'title' => 'Git',
+        'image' => 7,
         'created' => date('Y-m-d H:i:s')
       ],
       [
         'title' => 'Composer',
+        'image' => 8,
         'created' => date('Y-m-d H:i:s')
       ],
       [
         'title' => 'Gulp',
+        'image' => 9,
         'created' => date('Y-m-d H:i:s')
       ],
       [
         'title' => 'Docker',
+        'image' => 10,
         'created' => date('Y-m-d H:i:s')
       ],
       [
         'title' => 'Ansible',
+        'image' => 11,
         'created' => date('Y-m-d H:i:s')
       ],
       [
         'title' => 'JQuery',
+        'image' => 12,
         'created' => date('Y-m-d H:i:s')
       ],
       [
         'title' => 'Bootstrap',
+        'image' => 13,
         'created' => date('Y-m-d H:i:s')
       ],
       [
         'title' => 'Foundation',
+        'image' => 14,
         'created' => date('Y-m-d H:i:s')
       ],
       [
         'title' => 'Laravel',
+        'image' => 16,
         'created' => date('Y-m-d H:i:s')
       ],
       [
         'title' => 'Symfony',
+        'image' => 17,
         'created' => date('Y-m-d H:i:s')
       ],
     ];
