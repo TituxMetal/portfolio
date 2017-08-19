4d376782a9b07b9c11ca3940778d51b1a3e40ad8
Tuxi Metal <lang.guillaume@gmail.com>
2017-08-19 08:08:12 +0200

    Adding the service for technologies data access

--- app/services.php
+++ app/services.php
@@ -3,6 +3,7 @@
 use Silex\Provider\DoctrineServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Tuxi\Portfolio\Repository\KnowledgeRepository;
+use Tuxi\Portfolio\Repository\TechnologyRepository;
 
 /*
  * Add the Doctrine service provider for database connection.
@@ -22,4 +23,12 @@ $app->register(new TwigServiceProvider(), [
 $app['dao.knowledge'] = function($app) {
   
   return new KnowledgeRepository($app['db']);
+};
+
+/*
+ * Add the Technology repository.
+ */
+$app['dao.technology'] = function($app) {
+  
+  return new TechnologyRepository($app['db']);
 };
