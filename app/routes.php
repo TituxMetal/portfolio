c2c2778560b07b63697e7d5adf79ccbe9e4da956
Tuxi Metal <lang.guillaume@gmail.com>
2017-08-20 11:13:42 +0200

    Updating the home route and getting projects data

--- app/routes.php
+++ app/routes.php
@@ -3,8 +3,11 @@
 $app->get('/', function() use($app) {
   $knowledges = $app['dao.knowledge']->findAll();
   $technologies = $app['dao.technology']->findAll();
+  $projects = $app['dao.project']->findAll();
   
-  return $app['twig']->render('index.html.twig',
-    ['knowledges' => $knowledges, 'technologies' => $technologies]
-  );
+  return $app['twig']->render('index.html.twig', [
+    'knowledges' => $knowledges,
+    'technologies' => $technologies,
+    'projects' => $projects
+  ]);
 });
