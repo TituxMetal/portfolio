31ad8c6a82357e5aa02fb2ad3c8d0d4fa9722127
Tuxi Metal <lang.guillaume@gmail.com>
2017-08-20 10:40:47 +0200

    Adding migration for projects table

---
+++ storage/migrations/20170820043759_create_projects_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class CreateProjectsTable extends AbstractMigration {
+  
+  public function up() {
+    $project = $this->table('projects');
+    $project->addColumn('title', 'string', ['limit' => 255])
+      ->addColumn('description', 'text', ['limit' => MysqlAdapter::TEXT_LONG])
+      ->addColumn('created', 'timestamp')
+      ->save();
+  }
+  
+  public function down() {
+    $this->dropTable('projects');
+  }
+  
+}
