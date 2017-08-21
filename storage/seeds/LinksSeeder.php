a2370a3d17af2d64c8de3ee45898df87a4c9e139
Tuxi Metal <lang.guillaume@gmail.com>
2017-08-21 11:32:59 +0200

    Adding seed data for links table

---
+++ storage/seeds/LinksSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class LinksSeeder extends AbstractSeed {
+    
+  public function run() {
+    $data = [
+      [
+        'href' => "http://tenchido.fr",
+        'label' => "Voir le projet",
+        'title' => "Karaté Tenchido Obernai après la refonte",
+        'created' => date('Y-m-d H:i:s')
+      ],
+      [
+        'href' => "https://web.archive.org/web/20161028195201/http://www.tenchido.fr/",
+        'label' => "Projet avant refonte",
+        'title' => "Karaté Tenchido Obernai avant la refonte",
+        'created' => date('Y-m-d H:i:s')
+      ],
+      [
+        'href' => "http://albinweyer.fr",
+        'label' => "Voir le projet",
+        'title' => "Page de bienvenue du jeune Albin",
+        'created' => date('Y-m-d H:i:s')
+      ],
+      [
+        'href' => "https://github.com/TituxMetal/welcomAlbin",
+        'label' => "Voir les sources",
+        'title' => "Code source de la page de bienvenue du jeune Albin",
+        'created' => date('Y-m-d H:i:s')
+      ],
+      [
+        'href' => "http://tictactoe.lg-ed.com",
+        'label' => "Voir le projet",
+        'title' => "Jeu du Tic Tac Toe",
+        'created' => date('Y-m-d H:i:s')
+      ],
+      [
+        'href' => "https://github.com/TituxMetal/ticTacToe",
+        'label' => "Voir les sources",
+        'title' => "Code source de la page de bienvenue du jeune Albin",
+        'created' => date('Y-m-d H:i:s')
+      ],
+      [
+        'href' => "http://notes.lg-ed.com",
+        'label' => "Voir le projet",
+        'title' => "App de prise de notes",
+        'created' => date('Y-m-d H:i:s')
+      ],
+      [
+        'href' => "https://github.com/TituxMetal/vuejsNotes",
+        'label' => "Voir les sources",
+        'title' => "Code source de l'pplication de prise de notes",
+        'created' => date('Y-m-d H:i:s')
+      ],
+    ];
+
+    $links = $this->table('links');
+    $links->insert($data)->save();
+  }
+    
+}
