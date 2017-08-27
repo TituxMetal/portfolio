ca210ff5c2a379d93f79ca22109bc3f68e1d1eb0
Tuxi Metal <lang.guillaume@gmail.com>
2017-08-27 09:17:20 +0200

    Adding a post contact route

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+
 $app->get('/', function() use($app) {
   $knowledges = $app['dao.knowledge']->findAll();
   $technologies = $app['dao.technology']->findAll();
@@ -11,3 +13,27 @@ $app->get('/', function() use($app) {
     'projects' => $projects
   ]);
 });
+
+$app->post('/contact', function(Request $request) use($app) {
+  $app['session']->clear();
+  $contactRepository = $app['dao.contact'];
+  $contact = $contactRepository->validate($request->request->all());
+  
+  if($contactRepository->hasErrors()) {
+    $app['session']->set('errors', $contactRepository->errors());
+    
+    return $app->redirect('/#contact');
+  }
+  
+  $app['mailer']
+    ->from($contact->email(), $contact->name())
+    ->send(new Tuxi\Portfolio\Mail\ContactMessage($contact));
+  
+  if($contactRepository->insert($contact)) {
+    $app['session']->getFlashBag()->add('success', "Votre message à bien été envoyé, merci !");
+    
+    return $app->redirect('/#contact');
+  }
+  
+  
+})->bind('contact');
