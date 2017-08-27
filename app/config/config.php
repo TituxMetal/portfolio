4f3d5bb72bc246c2f07866caee740335d70cb1a1
Tuxi Metal <lang.guillaume@gmail.com>
2017-08-27 09:07:47 +0200

    Updating dependencies and settings for Swift Mailer

--- app/config/config.php
+++ app/config/config.php
@@ -10,5 +10,17 @@ return [
     'user' => getenv('DB_USERNAME'),
     'password' => getenv('DB_PASSWORD'),
   ],
+  'mail' => [
+    'host' => getenv('MAIL_HOST'),
+    'port' => getenv('MAIL_PORT'),
+    'username' => getenv('MAIL_USERNAME'),
+    'password' => getenv('MAIL_PASSWORD'),
+    'auth_mode' => getenv('MAIL_AUTH'),
+    'encryption' => getenv('MAIL_ENC'),
+    'from' => [
+      'address' => getenv('MAIL_FROM_ADDRESS'),
+      'name' => getenv('MAIL_FROM_NAME')
+    ],
+  ],
   'debug' => getenv('APP_DEBUG')
 ];
