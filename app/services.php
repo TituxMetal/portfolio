5b463caf7b31dfbf28cb76b8b2e4445d8ebb4b04
Tuxi Metal <lang.guillaume@gmail.com>
2017-08-27 09:09:35 +0200

    Updating services; add session provider, contact repository and mailer class

--- app/services.php
+++ app/services.php
@@ -5,7 +5,8 @@ use Silex\Provider\TwigServiceProvider;
 use Tuxi\Portfolio\Repository\{
   KnowledgeRepository,
   TechnologyRepository,
-  ProjectRepository
+  ProjectRepository,
+  ContactRepository
 };
 
 /*
@@ -19,6 +20,23 @@ $app->register(new DoctrineServiceProvider());
 $app->register(new TwigServiceProvider(), [
   'twig.path' => dirname(__DIR__) . '/resources/templates'
 ]);
+
+$app->register(new Silex\Provider\SessionServiceProvider());
+
+$app['mailer'] = function($app) {
+  $config = $app['mail'];
+  
+  $transport = (new Swift_SmtpTransport($config['host'], $config['port']))
+    ->setUsername($config['username'])
+    ->setPassword($config['password'])
+    ->setAuthMode($config['auth_mode'])
+    ->setEncryption($config['encryption']);
+  
+  $swift = new Swift_Mailer($transport);
+  
+  return (new \Tuxi\Portfolio\Mail\Mailer\Mailer($swift, $app['twig']))
+    ->alwaysTo($config['from']['address'], $config['from']['name']);
+};
 
 /*
  * Add the Knowledge repository.
@@ -42,4 +60,12 @@ $app['dao.technology'] = function($app) {
 $app['dao.project'] = function($app) {
   
   return new ProjectRepository($app['db']);
+};
+
+/*
+ * Add the Contact repository.
+ */
+$app['dao.contact'] = function($app) {
+  
+  return new ContactRepository($app['db']);
 };
