fdcc5197232080dd68440782f579f0545c6e90ff
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2017-09-21 12:09:55 +0200

    Reorganizing the controller from routes file in the HomeController file

--- app/routes.php
+++ app/routes.php
@@ -1,46 +1,9 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
-
 // The homepage route
-$app->get('/', function() use($app) {
-  $knowledges = $app['dao.knowledge']->findAll();
-  $technologies = $app['dao.technology']->findAll();
-  $projects = $app['dao.project']->findAll();
-  
-  return $app['twig']->render('index.html.twig', [
-    'knowledges' => $knowledges,
-    'technologies' => $technologies,
-    'projects' => $projects
-  ]);
-});
+$app->get('/', "Tuxi\Portfolio\Controllers\HomeController::home")
+  ->bind('home');
 
 // The post route for contact form
-$app->post('/contact', function(Request $request) use($app) {
-  $contactRepository = $app['dao.contact'];
-  $contact = $contactRepository->validate($request->request->all());
-  
-  if($contactRepository->hasErrors()) {
-    $app['session']->getFlashBag()->add(
-      'errors',
-      $contactRepository->errors()
-    );
-    
-    return $app->redirect('/#contact');
-  }
-  
-  $app['mailer']
-    ->from($contact->email(), $contact->name())
-    ->send(new Tuxi\Portfolio\Mail\ContactMessage($contact));
-  
-  if($contactRepository->insert($contact)) {
-    $app['session']->getFlashBag()->add(
-      'success',
-      "Votre message a bien été envoyé, merci !"
-    );
-    
-    return $app->redirect('/#contact');
-  }
-  
-  
-})->bind('contact');
+$app->post('/contact', "Tuxi\Portfolio\Controllers\HomeController::contact")
+  ->bind('contact');
