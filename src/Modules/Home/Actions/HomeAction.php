5c1d8df4f3c709780e4869a305d1174f990be9cf
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2017-11-06 08:01:09 +0100

    Updating the home module

--- src/Modules/Home/Actions/HomeAction.php
+++ src/Modules/Home/Actions/HomeAction.php
@@ -2,7 +2,9 @@
 
 namespace Portfolio\Modules\Home\Actions;
 
+use PDO;
 use Portfolio\Core\Templating\RendererInterface;
+use Portfolio\Modules\Knowledge\Table\KnowledgeTable;
 
 /**
  * Description of HomeAction
@@ -12,16 +14,30 @@ use Portfolio\Core\Templating\RendererInterface;
 class HomeAction {
 
   /**
+   * @var KnowledgeTable
+   */
+  private $table;
+
+  /**
+   * @var PDO
+   */
+  private $pdo;
+
+  /**
    * @var RendererInterface
    */
   private $renderer;
 
-  public function __construct(RendererInterface $renderer) {
+  public function __construct(RendererInterface $renderer, PDO $pdo, KnowledgeTable $table) {
     $this->renderer = $renderer;
+    $this->pdo = $pdo;
+    $this->table = $table;
   }
   
   public function __invoke(): string {
+    $knowledges = $this->table
+      ->findForHome();
     
-    return $this->renderer->render('@home/index');
+    return $this->renderer->render('@home/index', compact('knowledges'));
   }
 }
