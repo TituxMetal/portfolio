3ac065972dca09a9ab2c65afbf16bf0b41154513
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2017-11-10 07:53:42 +0100

    Updating migrations and seeds in technology module; adding the relation with pictures table

---
+++ src/Modules/Technology/storage/migrations/20171106213709_add_picture_to_technologies_table.php
@@ -0,0 +1,14 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddPictureToTechnologiesTable extends AbstractMigration {
+  
+  public function change() {
+    $this->table('Technologies')
+      ->addColumn('pictureId', 'integer', ['after' => 'name'])
+      ->addIndex('pictureId')
+      ->addForeignKey('pictureId', 'Pictures', 'id')
+      ->update();
+  }
+}
--- src/Modules/Technology/storage/seeds/TechnologiesSeeder.php
+++ src/Modules/Technology/storage/seeds/TechnologiesSeeder.php
@@ -5,20 +5,32 @@ use Phinx\Seed\AbstractSeed;
 class TechnologiesSeeder extends AbstractSeed {
   
   public function run() {
-    $data = [];
+    $pData = [];
+    $tData = [];
     $faker = Faker\Factory::create('fr_FR');
     
     for ($i = 1; $i <= 100; ++$i) {
       $date = $faker->unixTime('now');
-      $data[] = [
+      $pData[] = [
+        'uri' => '/assets/img/tech/php_logo.png',
+        'title' => $faker->catchPhrase,
+        'created' => date('Y-m-d H:i:s', $date),
+        'updated' => date('Y-m-d H:i:s', $date)
+      ];
+      $tData[] = [
         'name' => $faker->catchPhrase,
+        'pictureId' => $i,
         'created' => date('Y-m-d H:i:s', $date),
         'updated' => date('Y-m-d H:i:s', $date)
       ];
     }
     
+    $this->table('Pictures')
+      ->insert($pData)
+      ->save();
+    
     $this->table('Technologies')
-      ->insert($data)
+      ->insert($tData)
       ->save();
   }
 }
