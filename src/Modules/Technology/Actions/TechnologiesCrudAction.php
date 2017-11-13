9bf94f5911216b5c04ea28e006d3b845c9ce6362
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2017-11-13 07:33:19 +0100

    Updating Crud action class for technologies

--- src/Modules/Technology/Actions/TechnologiesCrudAction.php
+++ src/Modules/Technology/Actions/TechnologiesCrudAction.php
@@ -7,6 +7,7 @@ use Portfolio\Core\Actions\CrudActions;
 use Portfolio\Core\Routing\Router;
 use Portfolio\Core\Sessions\FlashService;
 use Portfolio\Core\Templating\RendererInterface;
+use Portfolio\Modules\Common\Table\PictureTable;
 use Portfolio\Modules\Technology\Entity\Technology;
 use Portfolio\Modules\Technology\Table\TechnologyTable;
 use Psr\Http\Message\ServerRequestInterface;
@@ -23,8 +24,13 @@ class TechnologiesCrudAction extends CrudActions {
    * @var array
    */
   protected $acceptedParams = [
-    'name', 'updated'
+    'name', 'pictureId', 'updated'
   ];
+  
+  /**
+   * @var PictureTable
+   */
+  private $pictureTable;
   
   protected $routePrefix = "admin.technologies";
   
@@ -37,28 +43,43 @@ class TechnologiesCrudAction extends CrudActions {
       RendererInterface $renderer,
       Router $router,
       TechnologyTable $table,
+      PictureTable $pictureTable,
       FlashService $flash
   ) {
     parent::__construct($renderer, $router, $table, $flash);
+    $this->pictureTable = $pictureTable;
   }
   
   protected function prePersist(ServerRequestInterface $request, $item): array {
     $now = date('Y-m-d H:i:s');
+    
     $requestParams = array_merge(
       $request->getParsedBody(),
       ['updated' => $now]
     );
     
-    if (is_null($item->getId())) {
-      $requestParams['created'] = $now;
-      $this->acceptedParams[] = 'created';
-    }
-    
-    $params = array_filter($requestParams, function ($key) {
-      return in_array($key, $this->acceptedParams);
+    $requestParams['created'] = $now;
+
+    $relatedParams = array_filter($requestParams, function ($key) {
+      return in_array($key, $this->pictureTable->fillable);
     }, ARRAY_FILTER_USE_KEY);
     
-    return $params;
+    if (!$item->getPictureId()) {
+      $this->pictureTable->fillable[] = 'created';
+      $this->pictureTable->insert($relatedParams);
+      $item->setPictureId($this->pictureTable->getLastInsertId());
+      $this->table->fillable[] = 'created';
+    } else {
+      unset($relatedParams['created']);
+      $this->pictureTable->update($item->getPictureId(), $relatedParams);
+    }
+    $requestParams['pictureId'] = $item->getPictureId();
+    
+    $currentParams = array_filter($requestParams, function ($key) {
+      return in_array($key, $this->table->fillable);
+    }, ARRAY_FILTER_USE_KEY);
+    
+    return $currentParams;
   }
   
   /**
@@ -76,6 +97,8 @@ class TechnologiesCrudAction extends CrudActions {
   protected function getValidator(ServerRequestInterface $request) {
     $validator = parent::getValidator($request)
       ->required('name')
+      ->required('title')
+      ->length('title', 3, 56)
       ->length('name', 3, 56);
     
     return $validator;
