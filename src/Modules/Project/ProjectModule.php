0dbbf213d48564f496960910f95ab246842804a2
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2017-11-27 09:07:45 +0100

    Fix #16: commented code should not be committed

--- src/Modules/Project/ProjectModule.php
+++ src/Modules/Project/ProjectModule.php
@@ -3,9 +3,6 @@
 namespace Portfolio\Modules\Project;
 
 use Portfolio\Core\Modules\Module;
-use Portfolio\Core\Routing\Router;
-use Portfolio\Core\Templating\RendererInterface;
-use Psr\Container\ContainerInterface;
 
 /**
  * Description of ProjectModule
@@ -19,18 +16,4 @@ class ProjectModule extends Module {
   const MIGRATIONS = __DIR__ . '/storage/migrations';
   
   const SEEDS = __DIR__ . '/storage/seeds';
-
-  public function __construct(ContainerInterface $container) {
-    /*
-    $technologyPrefix = $container->get('project.prefix');
-    $container->get(RendererInterface::class)->addPath('projects', __DIR__ . '/resources/templates');
-    
-    $router = $container->get(Router::class);
-    
-    if ($container->has('admin.prefix')) {
-      $adminPrefix = $container->get('admin.prefix');
-      $router->crud($adminPrefix . $technologyPrefix, ProjectCrudAction::class, 'admin.projects');
-    }
-    */
-  }
 }
