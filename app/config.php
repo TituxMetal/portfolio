155a79a2d4a30ab2ec1843ff2ea8f8e759120d3f
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2017-11-27 08:40:20 +0100

    Updating the app config; adding config for mail

--- app/config.php
+++ app/config.php
@@ -5,25 +5,36 @@ use Portfolio\Core\Sessions\PhpSession;
 use Portfolio\Core\Sessions\SessionInterface;
 use Portfolio\Core\Templating\RendererInterface;
 use Portfolio\Core\Templating\TwigRendererFactory;
+use Portfolio\Core\TwigExtensions\FlashExtension;
 use Portfolio\Core\TwigExtensions\FormExtension;
 use Portfolio\Core\TwigExtensions\PagerfantaExtension;
 use Portfolio\Core\TwigExtensions\RouterTwigExtension;
 use Psr\Container\ContainerInterface;
+use function DI\env;
 use function DI\factory;
 use function DI\get;
 use function DI\object;
 
 return [
-  'database.host' => 'localhost',
-  'database.username' => 'tuxi',
-  'database.password' => '',
-  'database.name' => 'portfolioTest',
+  'env' => env('ENV', 'prod'),
+  'database.host' => env('DB_HOST', 'localhost'),
+  'database.username' => env('DB_USER', 'root'),
+  'database.password' => env('DB_PASS', 'root'),
+  'database.name' => env('DB_NAME', 'database'),
+  'mail.to' => env('MAIL_TO', 'admin@local.dev'),
+  'mail.host' => env('MAIL_HOST', 'localhost'),
+  'mail.port' => env('MAIL_PORT', 1025),
+  'mail.username' => env('MAIL_USER', null),
+  'mail.password' => env('MAIL_PASS', null),
+  'mail.auth' => env('MAIL_AUTH', null),
+  'mail.encryption' => env('MAIL_ENC', null),
   'migrations.path' => dirname(__DIR__) . '/storage/migrations',
   'seeds.path' => dirname(__DIR__) . '/storage/seeds',
   'views.path' => dirname(__DIR__) . '/resources/templates',
   'twig.extensions' => [
     get(PagerfantaExtension::class),
     get(FormExtension::class),
+    get(FlashExtension::class),
     get(RouterTwigExtension::class)
   ],
   \PDO::class => function (ContainerInterface $container) {
