e9faaf7e1e99325d4758af4ed119081d77943c18
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-01-02 11:34:07 +0100

    Remove dist folder

--- app/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// The homepage route
-$app->get('/', "Tuxi\Portfolio\Controllers\HomeController::home")
-  ->bind('home');
-
-// The post route for contact form
-$app->post('/contact', "Tuxi\Portfolio\Controllers\HomeController::contact")
-  ->bind('contact');
--- public/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require dirname(__DIR__) . '/app/bootstrap.php';
-
-$app->run();
